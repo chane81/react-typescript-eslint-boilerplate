c128bd5644ac058ffc10d29b46cbfa3e650308f6
BM <chane81@naver.com>
2021-03-26 14:23:13 +0900

    typescript react app eslint first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  console.log('1');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
+        <button type='button' onClick={handleClick} />
       </header>
     </div>
   );
---
+++ src/next.config.js
@@ -0,0 +1,8 @@
+const aa = {
+  aa: 1,
+  bb: {
+    test: '23',
+  },
+};
+
+console.log('aa', aa);
